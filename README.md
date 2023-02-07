fc026c1e4d57e51a51ce9f9e345f424ae46f9d4a
faith nchifor <faithnchifor@gmail.com>
2023-02-07 16:40:27 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-## Hellooo everyone!
-I am a software Developer and a Data Enthusiast. I am commited to what I do. I believe in making the positive difference
--- src/pages/notFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NotFound() {
-  return (
-    <div className='mh-100'>
-        <p className='text-center fs-2 fw-bold'>Page not found :(</p>
-    </div>
-  )
-}
